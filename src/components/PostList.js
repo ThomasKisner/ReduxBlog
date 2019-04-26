4cb746b0f47ed1a55eb8d3c3f76254d9ef1df351
thomaskisner <tjkisner@gmail.com>
2019-04-26 16:02:58 -0400

    fixed spelling error causing semantic ui cdn to not load

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,7 +6,7 @@ import UserHeader from "./UserHeader";
 
 class PostList extends Component {
   componentDidMount() {
-    this.props.fetchPostsAndUsers(); 
+    this.props.fetchPostsAndUsers();
   }
 
   renderList() {
@@ -14,21 +14,25 @@ class PostList extends Component {
       return (
         <div className="item" key={post.id}>
           <i className="large middle aligned icon user" />
-          
+
           <div className="content">
             <div className="description">
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
-            <UserHeader userId={post.userId}/>
+            <UserHeader userId={post.userId} />
           </div>
         </div>
       );
     });
   }
 
-  render() { 
-    return <div className="ui relaxed divided list">{this.renderList()}</div>;
+  render() {
+    return (
+      <div className="ui container">
+        <div className="ui relaxed divided list">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
